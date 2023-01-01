a53a00c6601fe88c802395b4e7c5bc5725532abe
Your Name <you@example.com>
2023-01-01 16:37:11 -0500

    working on spinner implementation

--- app/src/main/java/com/example/media/HomeFragment.java
+++ app/src/main/java/com/example/media/HomeFragment.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,6 +16,7 @@ import androidx.fragment.app.Fragment;
 import com.example.media.databinding.FragmentHomeBinding;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
@@ -29,9 +31,23 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         receiver = (MainReceiver) getActivity();
 
+        final String[] options = { "Foo", "Bar", "Baz" };
+        Spinner spinner = (Spinner) getActivity().findViewById(R.id.spinner);
+
+        ArrayList<SpinnerItem> spinnerItems = new ArrayList<>();
+
+        for (int i = 0; i < options.length; i++) {
+            SpinnerItem spinnerItem = new SpinnerItem();
+            spinnerItem.setTitle(options[i]);
+            spinnerItem.setSelected(false);
+            spinnerItems.add(spinnerItem);
+        }
+        CustomAdapter customAdapter = new CustomAdapter(getActivity(), 0, spinnerItems);
+        spinner.setAdapter(customAdapter);
+
         File newList[] = receiver.getNewList();
 
-        Log.v("YYY", String.valueOf(newList.length));
+        //Log.v("YYY", String.valueOf(newList.length));
         if (newList.length > 0) {
             View tag = inflater.inflate(R.layout.tag, null);
             TextView textView = tag.findViewById(R.id.text);
@@ -46,8 +62,6 @@ public class HomeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         //binding.tagholder
-
-
 
 //        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
 //            @Override
