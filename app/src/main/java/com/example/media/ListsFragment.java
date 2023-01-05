96b29bc4fbd384a87bdc6149a074da413bdd13b1
Your Name <you@example.com>
2023-01-04 20:31:01 -0500

    working on adding tags and lists

--- app/src/main/java/com/example/media/ListsFragment.java
+++ app/src/main/java/com/example/media/ListsFragment.java
@@ -4,38 +4,62 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.media.databinding.FragmentListsBinding;
-
+import java.util.ArrayList;
 
 public class ListsFragment extends Fragment {
 
+    private MainReceiver receiver;
+    private DBManager dbManager;
+    public ArrayAdapter arrayAdapter;
     private FragmentListsBinding binding;
+    private ArrayList<String> listsList;
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-
         binding = FragmentListsBinding.inflate(inflater, container, false);
+        receiver = (MainReceiver) getActivity();
+        dbManager = receiver.getDBManager();
         return binding.getRoot();
-
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        String query = "SELECT name FROM lists ORDER BY name ASC";
+        ArrayList<Object> listsList = dbManager.fetch(query, null, "name");
+
+        // set the lists list
+        arrayAdapter = new ArrayAdapter(getActivity(), R.layout.text_item, listsList);
+        binding.listslist.setAdapter(arrayAdapter);
+
+        // add
         binding.addlist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Toast.makeText(getContext(), "Lists!", Toast.LENGTH_SHORT).show();
                 NavHostFragment.findNavController(ListsFragment.this)
                         .navigate(R.id.action_ListsFragment_to_AddListFragment);
+            }
+        });
+
+        // edit
+        binding.listslist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long id) {
+                Bundle bundle = new Bundle();
+                bundle.putString("listName", listsList.get(pos).toString());
+                NavHostFragment.findNavController(ListsFragment.this)
+                        .navigate(R.id.action_ListsFragment_to_EditListFragment, bundle);
             }
         });
     }
@@ -45,5 +69,4 @@ public class ListsFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
-
 }
--- app/src/main/java/com/example/media/TagsFragment.java
+++ app/src/main/java/com/example/media/TagsFragment.java
@@ -4,41 +4,61 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.media.databinding.FragmentTagsBinding;
+import java.util.ArrayList;
 
 public class TagsFragment extends Fragment {
 
+    private MainReceiver receiver;
+    private DBManager dbManager;
+    public ArrayAdapter arrayAdapter;
     private FragmentTagsBinding binding;
+    private ArrayList<String> listsList;
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-
         binding = FragmentTagsBinding.inflate(inflater, container, false);
+        receiver = (MainReceiver) getActivity();
+        dbManager = receiver.getDBManager();
         return binding.getRoot();
-
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        String query = "SELECT name FROM tags ORDER BY name ASC";
+        ArrayList<Object> tagsList = dbManager.fetch(query, null, "name");
+
+        // set the lists list
+        arrayAdapter = new ArrayAdapter(getActivity(), R.layout.text_item, tagsList);
+        binding.tagslist.setAdapter(arrayAdapter);
+
+        // add
         binding.addtag.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Toast.makeText(getContext(), "Tags!", Toast.LENGTH_SHORT).show();
-                //NavHostFragment.findNavController(TagsFragment.this)
-                //        .navigate(R.id.action_TagsFragment_to_AddTagFragment);
-                //Navigation.findNavController(view).navigate(R.id.viewTransactionsAction);
                 NavHostFragment.findNavController(TagsFragment.this)
                         .navigate(R.id.action_TagsFragment_to_AddTagFragment);
+            }
+        });
 
+        // edit
+        binding.tagslist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long id) {
+                Bundle bundle = new Bundle();
+                bundle.putString("tagName", tagsList.get(pos).toString());
+                NavHostFragment.findNavController(TagsFragment.this)
+                        .navigate(R.id.action_TagsFragment_to_EditTagFragment, bundle);
             }
         });
     }
@@ -48,5 +68,4 @@ public class TagsFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
-
 }
